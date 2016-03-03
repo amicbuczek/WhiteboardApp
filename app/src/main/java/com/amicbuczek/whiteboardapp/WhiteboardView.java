c960452dd775e8f74d1965cc9bfedd8e7a295232
buczek <buczek@email.arizona.edu>
2016-03-03 12:50:21 -0700

    Initial UI is set up. Drawing a line works in the canvas works. NOTE: starting to draw outside of canvas into canvas does not work. Desired functionality?

---
+++ app/src/main/java/com/amicbuczek/whiteboardapp/WhiteboardView.java
@@ -0,0 +1,109 @@
+package com.amicbuczek.whiteboardapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by buczek on 3/3/16.
+ */
+public class WhiteboardView extends View {
+
+    private Path path;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = Color.RED;
+    private Canvas canvas;
+    private Bitmap bitmap;
+
+    public WhiteboardView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        setUpWhiteboard();
+    }
+
+    /**
+     * Set up the drawing area for the user interaction
+     */
+    private void setUpWhiteboard() {
+        path = new Path();
+        drawPaint = new Paint(paintColor);
+
+        //Smooths out the edges of what is being drawn
+        drawPaint.setAntiAlias(true);
+
+        drawPaint.setStrokeWidth(getResources().getDimension(R.dimen.brush_medium));
+        drawPaint.setStyle(Paint.Style.STROKE);
+
+        //This will create a smoother circular arc when outer edges meet
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+
+        //This will end the paint path in a semicircle
+        //Alternatively BUTT -- stroke ends with path
+        //Square -- ends as a square shape
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    /**
+     * This method is called when the view is assigned a size.
+     * Instantiating the canvas and bitmap with the view size.
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
+
+    /**
+     * This method allows the custom class to function as a custom
+     * drawing view.
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, canvasPaint);
+        canvas.drawPath(path, drawPaint);
+    }
+
+    /**
+     * Detecting anytime the user is touching the whiteboard
+     * view. Determine the touch location, the current touch action,
+     * and draw the path created by the touch.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            //The touch has just begun, start the path at this location
+            path.moveTo(touchX, touchY);
+        } else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            //Continue the path along this line
+            path.lineTo(touchX, touchY);
+        } else if(event.getAction() == MotionEvent.ACTION_UP ||
+                event.getAction() == MotionEvent.ACTION_CANCEL) {
+            //The drawing is complete, draw the path on the canvas
+            //with the current paint color, and reset the path
+            canvas.drawPath(path, drawPaint);
+            path.reset();
+        }else{
+            //Something unexpected happened, do not draw the path
+            return false;
+        }
+
+        //refresh the view
+        invalidate();
+        return true;
+    }
+}
